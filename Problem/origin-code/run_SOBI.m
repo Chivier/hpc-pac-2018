5e4aac6471a9131db539e175efb0a6ec7778e958
Chivier <chivier.humber@gmail.com>
2019-12-14 20:26:40 +0800

    Add time measure at origin code.

--- Problem/origin-code/run_SOBI.m
+++ Problem/origin-code/run_SOBI.m
@@ -1,8 +1,10 @@
-
-
 load DATA
 
-W = SOBI( DATA,  [0,1:10,12:2:20,25:5:100,125:25:350],  1000,  1e-5 );
-
+st_time = cputime
+fprintf('start at %d seconds\n', st_time);
+W = SOBI(DATA, [0, 1:10, 12:2:20, 25:5:100, 125:25:350], 1000, 1e-5);
+ed_time = cputime
+fprintf('end at %d seconds\n', ed_time);
+fprintf('duration = %d seconds\n', ed_time - st_time);
 %core code of SOBI function comes from Jean-Francoi s Cardoso, with
 %modifications by various individuals worked in Prof Akaysha Tang's labs
