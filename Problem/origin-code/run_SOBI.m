f6557bd6f1cc01f510596ce9e40a64fae4675eef
Chivier <chivier.humber@gmail.com>
2019-12-17 15:33:52 +0800

    Better format & time record.

--- Problem/origin-code/run_SOBI.m
+++ Problem/origin-code/run_SOBI.m
@@ -1,6 +1,7 @@
 load DATA
 
 st_time = cputime
+tic
 fprintf('start at %d seconds\n', st_time);
 W = SOBI(DATA, [0, 1:10, 12:2:20, 25:5:100, 125:25:350], 1000, 1e-5);
 ed_time = cputime
@@ -8,3 +9,4 @@ fprintf('end at %d seconds\n', ed_time);
 fprintf('duration = %d seconds\n', ed_time - st_time);
 %core code of SOBI function comes from Jean-Francoi s Cardoso, with
 %modifications by various individuals worked in Prof Akaysha Tang's labs
+toc
